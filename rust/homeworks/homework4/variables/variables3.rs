71440057a777d6c394024f467f97144fde18696c
Frank He <tahe.nft@gmail.com>
2023-04-27 17:48:28 +0000

    complete hw4 variables section

--- rust/homeworks/homework4/variables/variables3.rs
+++ rust/homeworks/homework4/variables/variables3.rs
@@ -1,9 +1,9 @@
 // variables3.rs
 // Make me compile! Execute the command `zustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
     let x: i32;
+    x = 100;
     println!("Number {}", x);
 }
