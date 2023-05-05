5c91a5ecf24b2ebada56f7816acb1d0c568f6c12
Frank He <tahe.nft@gmail.com>
2023-05-05 23:29:35 +0000

    complete the Rust hw

--- rust/homeworks/homework4/functions/functions2.rs
+++ rust/homeworks/homework4/functions/functions2.rs
@@ -1,13 +1,13 @@
 // functions2.rs
 // Make me compile! Execute `zustlings hint functions2` for hints :)
 
-// I AM NOT DONE
+
 
 fn main() {
     call_this(3);
 }
 
-fn call_this(num:) {
+fn call_this(num:u32) {
     for i in 0..num {
         println!("Loop! number {}", i + 1);
     }
